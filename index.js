19028bed0ba91cdb303d0003b5637f0a9cb3dba2
sivaranjansahu <sivaranjan.sahu@gmail.com>
2019-05-31 09:19:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let strings = [
+  "¡Llame una ambulancia!",
+  "Me duele el pie.",
+  "La Ambulancia",
+  "Me siento débil.",
+  "Tengo fiebre.",
+  "Tengo dolores.",
+  "Sufro de insomnio.",
+  "Tengo fiebre del heno",
+  "Me duele la rodilla.",
+  "No me siento bien.",
+  "Necesito una receta médica.",
+  "El hospital",
+  "¿Tiene fiebre?",
+  "Tengo una infección.",
+  "Tengo un virus.",
+  "Necesito muletas.",
+  "Me duele el ojo.",
+  "Me duele el hombro.",
+  "¡Necesito un médico!"
+];
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+strings.map((d, i) => {
+  let seed = Math.round(Math.random()) + Math.random();
+  let textelement = document.createTextNode(d);
+
+  let hElement = document.createElement("h1");
+  hElement.classList.add("word");
+  hElement.appendChild(textelement);
+  hElement.style.webkitFilter = "blur(" + seed * 1.5 + "px)";
+
+  let textcontainer = document.createElement("div");
+  textcontainer.classList.add("inside");
+  textcontainer.appendChild(hElement);
+  textcontainer.style.position = "absolute";
+  textcontainer.style.width = "200px";
+  textcontainer.style.left = Math.random() * (innerWidth - 100)+ 100+ "px";
+  textcontainer.style.top = Math.random() * (innerHeight - 100)+ 100 + "px";
+  //textcontainer.classList.add('speech-bubble');
+
+  let topbox = document.createElement("div");
+  topbox.setAttribute("data-depth", 1 - seed);
+  topbox.appendChild(textcontainer);
+
+  document.getElementById("scene").appendChild(topbox);
+});
+
+//Tweens
+//TweenMax.from(".inside", 1, { y: 100 });
+TweenMax.staggerFrom(".inside", 1, { y: 50, opacity: 0 }, 0.2);
+
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene, {
+  scalarX: 3,
+  scalarY: 3
+});
+
+$(document).ready(function() {
+  $("button").click(function() {
+    $("h1").shuffleLetters();
+  });
+});
