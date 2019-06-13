60ce799be79de4fdcaec4c424bf19d8834226069
Sivaranjan Sahu <sivaranjan@outlook.com>
2019-06-13 10:26:05 -0700

    sda

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ strings.map((d, i) => {
   let hElement = document.createElement("h1");
   hElement.classList.add("word");
   hElement.appendChild(textelement);
-  hElement.style.webkitFilter = "blur(" + (seed+0.6) * 2 + "px)";
+  hElement.style.webkitFilter = "blur(" + (seed+0.2) * 2 + "px)";
   hElement.style.fontSize = (1-seed)*12 + 12 + 'px';
 
   let textcontainer = document.createElement("div");
@@ -39,7 +39,7 @@ strings.map((d, i) => {
   textcontainer.style.position = "absolute";
   textcontainer.style.width = "200px";
   textcontainer.style.left = Math.random() * (innerWidth - 100)+ 100+ "px";
-  textcontainer.style.top = Math.random() * (innerHeight - 100)+ 100 + "px";
+  textcontainer.style.top = Math.random() * (700)+ 100 + "px";
   //textcontainer.classList.add('speech-bubble');
 
   let topbox = document.createElement("div");
@@ -50,18 +50,26 @@ strings.map((d, i) => {
 });
 
 //Tweens
-//TweenMax.from(".inside", 1, { y: 100 });
-TweenMax.staggerFrom(".inside", 1, { y: 50, opacity: 0 }, 0.2);
-TweenMax.staggerTo(".inside", 40, { y: -100 }, 0.2);
+var tl = new TimelineMax();
+tl.add( TweenMax.staggerFrom(".inside", 3, { y: 100, opacity: 0,ease: Elastic.easeOut.config(1,1) }, 0.1));
+tl.add( TweenMax.staggerTo(".inside", 100, {
+  
+  cycle:{
+    x:function() { return (Math.random() > 0.5 ? 1 : -1 ) * 100; },
+    y:function() { return (Math.random() > 0.5 ? 1 : -1 ) * 100; }
+  },
+  
+  }, 0.2));
 
 var scene = document.getElementById("scene");
 var parallaxInstance = new Parallax(scene, {
-  scalarX: 3,
-  scalarY: 3
+  scalarX: 1,
+  scalarY: 1,
+  relativeInput:true
 });
 
 $(document).ready(function() {
   $("button").click(function() {
-    $("h1").shuffleLetters();
+    $("h1.word").shuffleLetters();
   });
 });
